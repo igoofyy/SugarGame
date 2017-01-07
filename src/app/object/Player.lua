801a3a2c753742d234df2b8072784b667ef8f1d1
igoofyy <mgoofyy@gmail.com>
2017-01-07 23:55:10 +0800

    change player sprite

--- src/app/object/Player.lua
+++ src/app/object/Player.lua
@@ -4,10 +4,21 @@ Player = class("Player", function ()
 end)
 
 
+local MATERIAL_DEFAULT = cc.PhysicsMaterial(0.0, 0.0, 0.0)
 function Player:ctor()
 
+    -- self:addAnimationCache()
 
+    local body = cc.PhysicsBody:createBox(self:getContentSize(), MATERIAL_DEFAULT)
+    body:setRotationEnable(false)
 
+    body:setCategoryBitmask(0x0111)
+    body:setContactTestBitmask(0x1111)
+    body:setCollisionBitmask(0x1001)
+
+    self:setTag(200)
+    self:setPhysicsBody(body)
+    self:getPhysicsBody():setGravityEnable(false) 
 end
 
 function Player:fly()
--- src/app/scenes/GameScene.lua
+++ src/app/scenes/GameScene.lua
@@ -2,7 +2,7 @@ local GameBackGroundLayer = require("app.layer.GameBackGroundLayer")
 local Player = require("app.object.Player")
 
 GameScene = class("GameScene", function()
-	return display.newScene("GameScene")
+	return display.newPhysicsScene("GameScene")
 end)
 
 function GameScene:ctor()
@@ -10,9 +10,9 @@ function GameScene:ctor()
 	-- self:configBackground()
 
 	-- -- 设置世界的物理引擎
- --    self.world = self:getPhysicsWorld()
- --    self.world:setGravity(cc.p(0, -92))
- --    self.world:setDebugDrawMask(cc.PhysicsWorld.DEBUGDRAW_ALL)
+    self.world = self:getPhysicsWorld()
+    self.world:setGravity(cc.p(0, -92))
+    self.world:setDebugDrawMask(cc.PhysicsWorld.DEBUGDRAW_ALL)
 
 	self.backGroundLayer = GameBackGroundLayer.new()
 		:setAnchorPoint(0,0)
@@ -25,11 +25,20 @@ end
 -- 事件注册
 function GameScene:eventRegister()
 
+	local function onContactBegin(contact)
+    	print("···")
+    	return true
+    end
+
 	-- 注册点击事件
 	self.backGroundLayer:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
 			return self:onTouch(event.name,event.x,event.y)
 		end)
 	self.backGroundLayer:setTouchEnabled(true)
+
+	local contactListener = cc.EventListenerPhysicsContact:create()
+    contactListener:registerScriptHandler(onContactBegin, cc.Handler.EVENT_PHYSICS_CONTACT_BEGIN)
+
 
 end
 
